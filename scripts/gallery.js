7cd004c3ba0aafae2b20fc6f741211dfa1819f36
Filip Koza <peacefilip1989@gmail.com>
2020-12-27 08:49:38 +0200

    дз8 версия 2

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,8 +1,37 @@
 import imageSources from './gallery-items.js';
 
 const allList = document.querySelector('.js-gallery');
+const lightbox = document.querySelector('.js-lightbox');
+const lightbox__image = document.querySelector('.lightbox__image');
+const lightbox__button = document.querySelector(
+  'button[data-action="close-lightbox"]',
+);
 
-imageSources.forEach(elem => {
+allList.addEventListener('click', maximizeImg);
+lightbox__button.addEventListener('click', closeAction);
+
+function maximizeImg(event) {
+  event.preventDefault();
+  const target = event.target;
+  if (target.nodeName !== 'IMG') return;
+  lightbox.classList.add('is-open');
+  allList.style.visibility = 'hidden';
+  document.body.style.overflow = 'hidden';
+  setActiveLink(target);
+}
+function setActiveLink(target) {
+  lightbox__image.src = target.dataset.source;
+  lightbox__image.alt = target.alt;
+}
+function closeAction() {
+  allList.style.visibility = 'visible';
+  document.body.style.overflow = 'visible';
+  lightbox__image.src = '';
+  lightbox__image.alt = '';
+  lightbox.classList.remove('is-open');
+}
+
+const markupArray = imageSources.map(elem => {
   const listItem = document.createElement('li');
   listItem.classList.add('gallery__item');
   const linkOfItem = document.createElement('a');
@@ -13,39 +42,8 @@ imageSources.forEach(elem => {
   image.src = elem.preview;
   image.setAttribute('data-source', elem.original);
   image.alt = elem.description;
-  allList.append(listItem);
   listItem.append(linkOfItem);
   linkOfItem.append(image);
+  return listItem;
 });
-const lightbox = document.querySelector('.js-lightbox');
-
-const lightbox__overlay = document.querySelector('.lightbox__overlay');
-
-const lightbox__content = document.querySelector('.lightbox__content');
-
-const lightbox__image = document.querySelector('.lightbox__image');
-
-allList.addEventListener('click', maximizeImg);
-function maximizeImg(event) {
-  event.preventDefault();
-  const target = event.target;
-  if (target.nodeName !== 'IMG') return;
-  lightbox.classList.add('is-open');
-  allList.style.visibility = 'hidden';
-  document.body.style.overflow = 'hidden';
-
-  setActiveLink(target);
-}
-function setActiveLink(target) {
-  lightbox__image.src = target.dataset.source;
-}
-const lightbox__button = document.querySelector(
-  'button[data-action="close-lightbox"]',
-);
-
-lightbox__button.addEventListener('click', event => {
-  allList.style.visibility = 'visible';
-  document.body.style.overflow = 'visible';
-
-  lightbox.classList.remove('is-open');
-});
+allList.append(...markupArray);
