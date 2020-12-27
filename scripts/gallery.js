f06a967952e4e87009c1f939c31ff2e7bd680e5a
Filip Koza <peacefilip1989@gmail.com>
2020-12-27 15:46:03 +0200

    + 2 дод пункти

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -6,9 +6,13 @@ const lightbox__image = document.querySelector('.lightbox__image');
 const lightbox__button = document.querySelector(
   'button[data-action="close-lightbox"]',
 );
+const lightbox__overlay = document.querySelector('.lightbox__overlay');
 
 allList.addEventListener('click', maximizeImg);
 lightbox__button.addEventListener('click', closeAction);
+lightbox__overlay.addEventListener('click', closeAction);
+window.addEventListener('keydown', escapeFunction);
+// window.addEventListener('keydown', goRight);
 
 function maximizeImg(event) {
   event.preventDefault();
@@ -19,6 +23,21 @@ function maximizeImg(event) {
   document.body.style.overflow = 'hidden';
   setActiveLink(target);
 }
+// function goRight({ key }) {
+//   if (key === 'ArrowRight') {
+//     console.log(`Entering ${key}`);
+//     goToNextPicture();
+//   }
+// }
+// function goToNextPicture() {
+//   let elemInFocus = imageSources.find(elem => {
+//     elem.original === lightbox__image.src;
+//   });
+//   console.log(elemInFocus);
+//   let index = imageSources.indexOf(elemInFocus);
+//   console.log(index);
+//   lightbox__image.src = imageSources[index + 2].original;
+// }
 function setActiveLink(target) {
   lightbox__image.src = target.dataset.source;
   lightbox__image.alt = target.alt;
@@ -29,6 +48,11 @@ function closeAction() {
   lightbox__image.src = '';
   lightbox__image.alt = '';
   lightbox.classList.remove('is-open');
+}
+function escapeFunction({ key }) {
+  if (key === 'Escape') {
+    closeAction();
+  }
 }
 
 const markupArray = imageSources.map(elem => {
